604516968b184c0877b5ed9046f310891a329573
bradpasleyac <69188029+bradpasleyac@users.noreply.github.com>
2021-07-16 20:58:44 +1000

    Update lib.php

--- lib.php
+++ lib.php
@@ -407,10 +407,6 @@ function printMainMenuButton() {
 
 function printLessonButton(string $lessonTitle, int $lessonid) {
     
-    println('<span class="btn rounded-pill lh-lg bg-secondary shadow-lg justify-content-center" pointer-event="none" aria-disabled="true">');
-        println('<button class="btn rounded-pill bg-secondary text-light lead '.LESSONBUTTONSTYLE.'" pointer-event="none" aria-disabled="true">&nbsp;'.$lessonTitle.'&nbsp;</button>');
-        println('</span>');
-        
     println('<form id="Lesson-'.$lessonTitle.'" method="post">');
     println('  <input type="hidden" name="screen" value="'.SCREENLESSONMENU.'">');
     println('  <input type="hidden" name="lessonID" value="'.$lessonid.'">');
