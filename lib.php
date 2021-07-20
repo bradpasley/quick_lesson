1a1e1faabb4052d3e7c5b9966dccfe7356549383
bradpasleyac <69188029+bradpasleyac@users.noreply.github.com>
2021-07-20 19:47:40 +1000

    Update lib.php

--- lib.php
+++ lib.php
@@ -339,10 +339,10 @@ function printJSONModulePage(int $lessonid, int $moduleid) {
     println("<script>");
     println("var conceptid = $conceptid;");
     println("const moduleJSON = ".getModuleJSON($lessonid, $moduleid).";");
-    println('document.getElementById("conceptID").innerHTML = "JSON Content("+conceptid+")";');
-    println('document.getElementById("moduleTitle").innerHTML = moduleJSON["0"].title;');
-    println('document.getElementById("conceptTitle").innerHTML = moduleJSON["1"].title;');
-    println('document.getElementById("content").innerHTML = moduleJSON["0"].content;');
+    println('document.getElementById("conceptID").innerHTML = "JSON Content ID ("+conceptid+")";');
+    println('document.getElementById("moduleTitle").innerHTML = "Mod TITLE " + moduleJSON["0"].title;');
+    println('document.getElementById("conceptTitle").innerHTML = "Con TITLE " + moduleJSON["1"].title;');
+    println('document.getElementById("content").innerHTML = "CONTENT " + moduleJSON["1"].content;');
     println('showNavigationButtons();');
     println('function showNavigationButtons() {');
     println('   var navButtons = "";');
