1b9effc44fd78a4eb91dffcc159b1f5e6632c02e
bradpasleyac <69188029+bradpasleyac@users.noreply.github.com>
2021-08-05 23:02:10 +1000

    Update lib.php

--- lib.php
+++ lib.php
@@ -444,7 +444,7 @@ function printRightArrowButton(string $pageTitle, int $screenType, string $butto
         println('<form id="rightArrowButton_'.$pageTitle.'" method="post">');
         println('  <input type="hidden" name="screen" value="'.$screenType.'">');
         println($middleText);
-        println('  <button type="submit" class="btn rounded-pill lh-lg bg-primary text-light shadow-lg" name="rightArrowButton_'.$pageTitle.'">'.$buttonText.'&nbsp;'.NEXTBUTTONICON.'</button>');
+        println('  <button type="submit" class="btn rounded-pill lh-lg text-light shadow-lg '.LESSONBACKGROUNDSTYLE.'" name="rightArrowButton_'.$pageTitle.'">'.$buttonText.'&nbsp;'.NEXTBUTTONICON.'</button>');
         println('</form>');
         if($isNextButton) println('</div>');//end of padding
     }
